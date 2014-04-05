d1a594d84e93c671f6344b55891d18c0b64494e1
newphew92 <terrenceko@outlook.com>
2014-04-05 03:39:32 -0400

    Version 1.0 Second functional CGI file

--- validate.c
+++ validate.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-char username[30];
-char password[30];
-char string[200];
-char* usernameInput;
-char* passwordInput;
-char* input;
-double inputLength;
+
 int valid=0;
+char username[200];
+char password[200];
+char* input;
+char buffer[100];
 char c;
 int a=0;
-
+char string[200];
+char usernameInput[200];
+char passwordInput[200];
 int main(void){
 
 //int n = atoi(getenv("CONTENT_LENGTH"));
@@ -31,25 +31,85 @@ int n = atoi(getenv("CONTENT_LENGTH"));
             if (c!='+' && c!='&')       //The ampersand is there for splitting string for sscanf
                 string[a]=c;
             else
-                string[a]=' ';
+                string[a]='=';
             a++;
         }
     }
     string[a] = '\0';
-sscanf(string,"username=%s password=%s",usernameInput,passwordInput);
 
+int b=0;
+int c=0;
+int x=0;
+int y=0;
+//This is why everybody fucking hates CGI with C, sscanf can't compile to CGI FFS.
+while (string[b]!='\0'){
+    if (string[b]=='='){
+        c++;
+    }
+    if (c==3){
+        if (string[b]!='='){
+        passwordInput[y]=string[b];
+        //printf("%c", string[b]);
+        y++;
+    }
+    }   
+    if (c==1){
+        if (string[b]!='='){
+        usernameInput[x]=string[b];
+        //printf("%c", string[b]);
+        x++;
+    }   
+    }
+    b++;
 
+}
+usernameInput[x]='\0';
+passwordInput[y]='\0';    
+//sscanf(string,"username=%s password=%s",usernameInput,passwordInput);
 
 file=fopen("members.ssv", "r");
 while (fgets(buffer, 100, file)!=NULL){
-	sscanf(buffer, "%s %s", username, password);
-	if (strcmp(username, usernameInput)==0 
-		&& strcmp(password, passwordInput)==0)
-	{
-		valid=1;
-		break;
-	}
+b=0;
+c=0;
+x=0;
+y=0;
+while (buffer[b]!='\n'){
+    if (buffer[b]==' '){
+        c++;
+    }
+    if (c==1){
+        if (buffer[b]!=' '){
+        password[y]=buffer[b];
+        //printf("%c", buffer[b]);
+        y++;
+    }
+    }   
+    if (c==0){
+        if (buffer[b]!=' '){
+        username[x]=buffer[b];
+        //printf("%c", buffer[b]);
+        x++;
+    }   
+    }
+    b++;
+
 }
+username[x]='\0';
+password[y]='\0';  
+    //sscanf(buffer, "%s %s", username, password);
+    if (strcmp(username, usernameInput)==0 
+        && strcmp(password, passwordInput)==0)
+    {
+        valid=1;
+        break;
+    }
+//printf("<p>user%s", username);
+//printf("|%s</p>", usernameInput);
+//printf("<p>%s", password);
+//printf("|%s\n</p>", passwordInput);
+}
+fclose(file); 
+
 
 if (valid==1){
 printf("<link rel=\"stylesheet\" href=\"surveyStyle.css\">");
@@ -75,7 +135,7 @@ else{
 	printf("<link rel=\"stylesheet\" href=\"surveyStyle.css\">");
 printf("<head>");
 printf("<link rel=\"shortcut icon\" href=\"http://i68.photobucket.com/albums/i7/newphew92/bagel_zps29e40f4c.jpg\">");
-printf("<h1>Username exists already");
+printf("<h1>Invalid username or password");
 printf("</h1>");
 printf("</head>");
 printf("<BODY>");
