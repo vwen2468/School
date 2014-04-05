a356ddfedb6e0b1bc42729131c39f79e3f25bc12
newphew92 <terrenceko@outlook.com>
2014-04-05 01:12:34 -0400

    Version 0.13

--- newuser.c
+++ newuser.c
@@ -8,6 +8,10 @@ char* usernameInput;
 char* passwordInput;
 char* input;
 int invalid=0;
+char buffer[100];
+char c;
+int a=0;
+char string[200];
 
 int main(void)
 {
@@ -17,7 +21,7 @@ char buffer[100];
 printf("%s%c%c\n", "Content-Type:text/html;charset=iso-8859-1", 13, 10);
 
 int n = atoi(getenv("CONTENT_LENGTH"));
-     
+   
     // Decode String
     while ((c = getchar()) != EOF && a<n)
     {
@@ -32,13 +36,14 @@ int n = atoi(getenv("CONTENT_LENGTH"));
     }
     string[a] = '\0';
 sscanf(string,"username=%s password=%s",usernameInput,passwordInput);
+printf("%s\n", usernameInput);
+printf("%s\n", passwordInput);
+/*
 
-
-
-//usernameInput="newphew92";
-//passwordInput="hash";
-file=fopen("members.ssv", "r");
+//usernameInput="bad";
+//file=fopen("members.ssv", "r");
 while (fgets(buffer, 100, file)!=NULL){
+	//printf("%s\n", buffer );
 	sscanf(buffer, "%s %s", username, password);
 	if (strcmp(username, usernameInput)==0){
 		invalid=1;
@@ -62,7 +67,7 @@ printf("<ul>\n");
 printf("<li><a href=\"http://www.cs.mcgill.ca/~vwen/ass4/login.html\">Click here to login</a></li>\n");
 printf("<li><a href=\"http://www.cs.mcgill.ca/~ctrinh2/ass4/welcome.html\">Click here to return home</a></li>\n");
 printf("</div>\n");
-printf("</BODY\n");
+printf("</BODY>\n");
 }
 else{
 file=fopen("members.ssv", "a");
@@ -83,7 +88,7 @@ printf("<ul>");
 printf("<li><a href=\"http://www.cs.mcgill.ca/~vwen/ass4/login.html\">Click here to login</a></li>");
 printf("<li><a href=\"http://www.cs.mcgill.ca/~ctrinh2/ass4/welcome.html\">Click here to return homet</a></li>");
 printf("</div>");
-printf("</BODY");
+printf("</BODY>");
 }
-	return 0;
+*/	return 0;
 }
